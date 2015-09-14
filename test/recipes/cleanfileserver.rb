d2a993832b483632a5033a07fec80e822da0353b
Jos Nijmeijer <Jos.Nijmeijer@ogd.nl>
2015-09-14 12:00:49 +0000

    reboot eruit

--- test/recipes/cleanfileserver.rb
+++ test/recipes/cleanfileserver.rb
@@ -29,7 +29,7 @@ powershell_script 'Install Windows-Server-Backup' do
 	not_if "(Get-WindowsFeature -Name Windows-Server-Backup).Installed"
 end
 
- 
+=begin
 powershell_script 'Restart-computer' do 
     code 'Restart-Computer'  
     guard_interpreter :powershell_script
