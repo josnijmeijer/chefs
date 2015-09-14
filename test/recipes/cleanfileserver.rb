69ce2222b70aee90f0deb8b725e3ed82ab794a0b
Jos Nijmeijer <Jos.Nijmeijer@ogd.nl>
2015-09-14 12:12:42 +0000

    using native powershell -restart to Add-WindowsFeature

--- test/recipes/cleanfileserver.rb
+++ test/recipes/cleanfileserver.rb
@@ -8,23 +8,24 @@
 # figure out what is actually needed and removing all clutter
 # domain join
 # logfiles/dynamic naming/path
-
+# todo: na installatie van fileservices is er een reboot nodig. hierdoor installeert ie de andere subfeatures niet goed. Oplossing nu: haal de check op installatie weg en draai het script voor de 2e keer. uitzoeken hoe dit in 1 powershell script te krijgen is
+# optie: eerst fileservices installeren dan reboot, dan alle subfeatures. Chef lifecycle events gebruiken? of powershell native features?
 
 powershell_script 'Install FS' do 
-	code 'Add-WindowsFeature FileAndStorage-Services -IncludeAllSubFeature -IncludeManagementTools -LogPath c:\users\administrator\.chef\installFS.log '	
+	code 'Add-WindowsFeature FileAndStorage-Services -IncludeAllSubFeature -IncludeManagementTools -LogPath c:\users\administrator\.chef\installFS.log -Restart'	
 	guard_interpreter :powershell_script
 	not_if "(Get-WindowsFeature -Name FileAndStorage-Services).Installed"
 end
 
 powershell_script 'Install FileServices Tools' do
-	code 'Add-WindowsFeature RSAT-File-Services -IncludeAllSubFeature -IncludeManagementTools -LogPath c:\users\administrator\.chef\installFSTools.log'
+	code 'Add-WindowsFeature RSAT-File-Services -IncludeAllSubFeature -IncludeManagementTools -LogPath c:\users\administrator\.chef\installFSTools.log -Restart'
 	guard_interpreter :powershell_script
 	not_if "(Get-WindowsFeature -name RSAT-File-Services).Installed"
 
 end
 
 powershell_script 'Install Windows-Server-Backup' do 
-	code 'Add-WindowsFeature Windows-Server-Backup -IncludeAllSubFeature -IncludeManagementTools -LogPath c:\users\administrator\.chef\installbackup.log '	
+	code 'Add-WindowsFeature Windows-Server-Backup -IncludeAllSubFeature -IncludeManagementTools -LogPath c:\users\administrator\.chef\installbackup.log -Restart'	
 	guard_interpreter :powershell_script
 	not_if "(Get-WindowsFeature -Name Windows-Server-Backup).Installed"
 end
@@ -34,7 +35,6 @@ powershell_script 'Restart-computer' do
     code 'Restart-Computer'  
     guard_interpreter :powershell_script
 end
-
 
 # service 'w3svc' do
 #	action [:enable, :start]
